ab47bf6f6eacc70ced17645371cbfc7947559744
Denislav Denov <denislavdenov@gmail.com>
2021-02-04 14:51:43 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,5 @@ resource "tfe_workspace" "main" {
   name         = "tfe-workspace-test2"
   organization = "ddenov"
   terraform_version = "0.14.5"
-  lifecycle {
-    ignore_changes = [vcs_repo]
-  }
+  
 }
